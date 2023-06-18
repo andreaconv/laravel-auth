6e30d084ce4005dc3cd060c8a777d7835e5edea3
andreaconv <andrea2convertini@gmail.com>
2023-06-18 23:51:32 +0200

    [change] text in description

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -22,7 +22,7 @@ class ProjectsTableSeeder extends Seeder
       $new_project = New Project;
       $new_project->name = $faker->words(3, true);
       $new_project->slug = Project::generateSlug($new_project->name);
-      $new_project->text = $faker->text();
+      $new_project->description = $faker->text();
       $new_project->category = $faker->words(2, true);
       $new_project->date_creation = $faker->date('Y_m_d');
 
