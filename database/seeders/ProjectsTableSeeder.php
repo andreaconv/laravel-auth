af022b3289eefb713eff0cf3b353f305bf2be69e
andreaconv <andrea2convertini@gmail.com>
2023-06-18 19:10:13 +0200

    [add] SEEDER, funzione genera slug e call generale

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
+
+
+class ProjectsTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run(Faker $faker)
+  {
+    for ($i=0; $i < 25 ; $i++) {
+
+      $new_project = New Project;
+      $new_project->name = $faker->words(3, true);
+      $new_project->slug = Project::generateSlug($new_project->name);
+      $new_project->text = $faker->text();
+      $new_project->category = $faker->words(2, true);
+      $new_project->date_creation = $faker->date('Y_m_d');
+
+      // dump($new_project);
+      $new_project->save();
+  }
+  }
+}
