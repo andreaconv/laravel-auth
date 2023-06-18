d01b96ba13fb6c7af2c571f162b1d542cc127038
andreaconv <andrea2convertini@gmail.com>
2023-06-18 20:01:50 +0200

    [add] query nel projectController

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
@@ -14,7 +15,8 @@ class ProjectController extends Controller
    */
   public function index()
   {
-    return view('admin.projects.index');
+    $projects = Project::all();
+    return view('admin.projects.index', compact('projects'));
   }
 
   /**
