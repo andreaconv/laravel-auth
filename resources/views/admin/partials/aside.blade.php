379a936a487d08bfb30aba6a11b4488c73d1c81d
andreaconv <andrea2convertini@gmail.com>
2023-06-18 17:35:50 +0200

    [add] lista in aside e relativo style

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,6 +1,12 @@
 <aside class="bg-dark text-white">
 
-  Lorem, ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quaerat obcaecati nobis officiis, pariatur eius asperiores? Eum quos voluptatibus natus. Accusantium ipsa accusamus libero repellat hic molestias quidem suscipit officia.
+<nav>
+  <ul>
+    <li><a href="#"><i class="fa-solid fa-chart-line"></i>Dashboard</a></li>
+    <li><a href="#"><i class="fa-solid fa-list-ul"></i>Projects</a></li>
+    <li><a href="#"><i class="fa-solid fa-square-plus"></i>New Project</a></li>
+  </ul>
+</nav>
 
 
 </aside>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>My portfolio</title>
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   {{-- Font Awesome --}}
